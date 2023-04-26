318e7ef41cafef79916cb0cbdad637e58348f1f1
Alex Parry <51757193+alexparry@users.noreply.github.com>
2023-04-26 10:55:51 +0100

    Changed on button press from pin 0 to logo

--- etch-a-sketch.py
+++ etch-a-sketch.py
@@ -7,7 +7,7 @@ https://github.com/alexparry/microbit-etch-a-sketch
 
 Draw an image by toggling the LEDs on and off using the buttons.
 The blinking LED shows which LED you are currently on.
-Press pin 0 to turn an LED on or off.
+Press the micro:bit logo to turn an LED on or off.
 Press the A button to change the X position and the B button to change the Y position.
 Shake to clear.
 """
